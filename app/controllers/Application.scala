6be16c0e648d8637db04787703f09cb260a8bcb6
Peter Ungvari <ungvari.mail@gmail.com>
2013-01-16 01:02:53 +0100

    primitive implementation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,9 @@
 package controllers
 
 import play.api.mvc._
+import hu.globit.octoclock.Octoclock
+import java.text.SimpleDateFormat
+import java.util.Date
 
 object Application extends Controller {
 
@@ -9,7 +12,12 @@ object Application extends Controller {
   }
 
   def time = Action {
-    Ok(views.html.index())
+    val oc = new Octoclock
+    val fullTime = oc.getTime
+    val days = fullTime.substring(3,8);
+    val hours = fullTime.substring(8,12);
+    val conventional = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date)
+    Ok(views.html.index(days, hours, conventional))
   }
 
 }
